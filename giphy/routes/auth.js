9ed6a93a186ce141ced63a018511e277d6a796f1
greg <gman.international@gmail.com>
2021-11-01 10:58:17 -0400

    2021_11_01_Hashed_passwords

--- giphy/routes/auth.js
+++ giphy/routes/auth.js
@@ -17,28 +17,41 @@ let refreshTokens = [];
 
 // AUTH ROUTES
 
+// LOGIN ROUTE
+
 router.post('/login', (req, res) => {
     // read username and password from request body
     const { username, password } = req.body;
-    console.log(req.body)
+    //console.log(req.body)
 
-    User.findOne({username: username, password: password}, (err, user) => {
+    User.findOne({ username: username }, (err, user) => {
         if(err || !user) {
             console.log('Auth/Sign in error' + err);
-            res.sendStatus(401);
+            res.status(401).json('user does not exist');
         } else {
-        const accessToken = jwt.sign({ id: user._id, username: user.username}, accessTokenSecret, { expiresIn: '120m' });
-        const refreshToken = jwt.sign({ id: user._id, username: user.username}, refreshTokenSecret);
-
-        refreshTokens.push(refreshToken);
-
-        res.json({
-            accessToken,
-            refreshToken
-        });
+            user.comparePassword(password, (error, isMatch) => {
+                if(error) {
+                    console.log(error);
+                }
+                if(isMatch) {
+                    const accessToken = jwt.sign({ id: user._id, username: user.username}, accessTokenSecret, { expiresIn: '120m' });
+                    const refreshToken = jwt.sign({ id: user._id, username: user.username}, refreshTokenSecret);
+            
+                    refreshTokens.push(refreshToken);
+            
+                    res.json({
+                        accessToken,
+                        refreshToken
+                    });
+                } else {
+                    res.status(400).json('invalid credentials')
+                }
+            })
         }
     })
 });
+
+// TOKEN ROUTE
 
 router.post('/token', (req, res) => {
     const { token } = req.body;
@@ -64,11 +77,33 @@ router.post('/token', (req, res) => {
     });
 });
 
+// LOGOUT ROUTE
+
 router.post('/logout', (req, res) => {
     const { token } = req.body;
     refreshTokens = refreshTokens.filter(t => t !== token);
 
     res.send("Logout successful");
 });
+
+// SIGNUP ROUTE
+
+router.post('/signup', (req, res) => {
+    const { username, password } = req.body;
+    
+    User.findOne({username: username}).then((user) => {
+        if(user) {
+            res.status(500).json('user exists');
+        } else {
+            User.create({
+                username,
+                password
+            })
+
+            res.status(200).json('user created');
+        }
+    })
+});
+
 
 export default router;
